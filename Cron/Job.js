a0327518e073562744719ca51bcd9fa7500113bf
Ultimatebot1 <ultimatebot313@gmail.com>
2025-06-01 22:32:12 -0700

    Create Job.js

---
+++ Cron/Job.js
@@ -0,0 +1,12 @@
+// cron/job.js
+
+const runBot = require('../trader');
+
+function startCronJob() {
+  setInterval(() => {
+    console.log(`[CRON] Running trader.js at ${new Date().toISOString()}`);
+    runBot();
+  }, 10000); // runs every 10 seconds
+}
+
+module.exports = startCronJob;
